1c048defbcfb81121515200231c4ca94b171f2e5
Martin Miglio <marmig0404@gmail.com>
2024-10-10 13:59:08 -0400

    ignore apk files

--- src/consts.py
+++ src/consts.py
@@ -5,6 +5,10 @@ import pathlib
 SCRIPT_DIRECTORY = pathlib.Path(__file__).parent
 OUTPUT_DIRECTORY = SCRIPT_DIRECTORY.parent / "out"
 VERSIONS_DIRECTORY = SCRIPT_DIRECTORY.parent / "versions"
+
+OUTPUT_DIRECTORY.mkdir(parents=True, exist_ok=True)
+VERSIONS_DIRECTORY.mkdir(parents=True, exist_ok=True)
+
 ASSETS_DIRECTORY = SCRIPT_DIRECTORY.parent / "assets"
 
 BASE_APK_NAME = "com.supercell.clashroyale"
